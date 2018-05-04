43f7167c00f0aa5498627a3d758489fc3b4dfce9
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-03 18:56:57 -0700

    Fix Expr program to parse from file or stdin

--- tour/ExprJoyRide.go
+++ tour/ExprJoyRide.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/TheGhostHuCodes/tDA4R/tour/parser"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		panic(fmt.Sprintf("Expected one commandline argument"))
-	}
-	arg := os.Args[1]
-
-	fs, err := antlr.NewFileStream(arg)
+func readFromStdin() (*antlr.InputStream, error) {
+	bytes, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		panic(fmt.Sprintf("ERROR: %s\n", err.Error()))
+		return nil, err
+	}
+	return antlr.NewInputStream(string(bytes)), nil
+}
+
+func main() {
+	var s antlr.CharStream
+	var err error
+	if len(os.Args) == 1 {
+		s, err = readFromStdin()
+		if err != nil {
+			panic(fmt.Sprintf("ERROR: %s\n", err.Error()))
+		}
+	} else if len(os.Args) == 2 {
+		s, err = antlr.NewFileStream(os.Args[1])
+		if err != nil {
+			panic(fmt.Sprintf("ERROR: %s\n", err.Error()))
+		}
+	} else {
+		panic(fmt.Sprintf("Expected zero or one commandline argument(s)"))
 	}
 
-	lexer := parser.NewExprLexer(fs)
+	lexer := parser.NewExprLexer(s)
 	tokens := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 
 	p := parser.NewExprParser(tokens)
